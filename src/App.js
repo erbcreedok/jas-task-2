a06997f99b92fc74c46cf1987354c4efb47eb66c
Yerbol Syzdyk <erbolsyzdyq@gmail.com>
2022-03-19 15:56:15 +0600

    timer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import React, { useEffect, useState } from 'react'
 import './App.css';
-import React, { useState } from 'react'
 import { fibonacci } from './functions';
 
+let interval;
 function App() {
   const [number, setNumber] = useState(null)
+  const [countdown, setCountdown] = useState(0)
 
   function handleChange(e) {
     setNumber(+e.target.value)
+  }
+
+  function startCountdown() {
+    let x = number
+    clearInterval(interval)
+    interval = setInterval(() => {
+      setCountdown(x)
+      x -= 1
+      if (x < 0) {
+        clearInterval(interval)
+      }
+    }, 1000)
   }
 
   return (
@@ -14,8 +28,9 @@ function App() {
       <div>
         <input onChange={handleChange} type="number" />
       </div>
+      <button onClick={startCountdown}>Start countdown</button>
       <div>
-        {fibonacci(number)}
+        {countdown}
       </div>
     </div>
   )
