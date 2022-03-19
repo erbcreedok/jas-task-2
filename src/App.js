eaba6936c49df203feba38595c3efede37b8bcc4
Yerbol Syzdyk <erbolsyzdyq@gmail.com>
2022-03-19 15:04:00 +0600

    practice2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import './App.css';
-import { tableData } from './data'
+import React, { useState } from 'react'
+import { isEven } from './functions';
 
 function App() {
-  
-  const data = tableData.filter((item) => item.instalment)
+  const [number, setNumber] = useState(null)
+
+  function handleChange(e) {
+    setNumber(+e.target.value)
+  }
+
   return (
-    <table>
-      {data.map((item, index) => (
-        <tr key={index}>
-          <td>{index}</td>
-          <td>{item.name}</td>
-          <td>{item.price}</td>
-          <td>{item.count}</td>
-          <td>{item.instalment ? 'v' : ''}</td>
-        </tr>
-      ))}
-    </table>
-  );
+    <div className='container'>
+      <div>
+        <input onChange={handleChange} type="number" />
+      </div>
+      <div>
+        { isEven(number) ? 'Четное' : 'Не четное'}
+      </div>
+    </div>
+  )
 }
 
 export default App;
